4abaad3dc7329c7369041999800780e9fa231fd3
jacquelinelira10 <90946173+jacquelinelira10@users.noreply.github.com>
2022-01-04 17:16:17 -0600

    Create dbconnector.js

---
+++ dbconnector.js
@@ -0,0 +1,33 @@
+const mariadb = require('mariadb');
+
+const config ={
+  host: 'localhost',
+  user: 'testwebserveruser',
+  password: 'testwebserverpass',
+  database: 'webserverdb',
+  connectionLimit: 5,
+  acquireTimeout: 300
+}
+
+class DBConnector{
+  dbconnector = mariadb.createPool(config);
+
+  async query(sQuery){
+    var conn = await this.dbconnector.getConnection();
+    var ret = null;
+    await conn.query(sQuery)
+      .then(data =>{
+        ret = data;
+        console.log(data);
+        conn.end()
+      })
+      .catch(err =>{
+        console.log(err)
+        conn.end()
+      })
+    return ret;
+  }
+}
+
+module.exports = new DBConnector();
+
